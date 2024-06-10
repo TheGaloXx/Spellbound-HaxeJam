7438b0d18126a29bb5a3de44b8d33945e7b41bfb
TheGalo X <clanero9000@gmail.com>
2024-06-09 23:44:19 -0300

    adjusted some init stuff

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Spellbound - HaxeJam
- Game made for the HaxeJam 2024: Summer Jam. Theme: Spellcraft.
+    Game made for the HaxeJam 2024: Summer Jam. 
+    Theme: Spellcraft.
--- source/Main.hx
+++ source/Main.hx
@@ -1,13 +1,18 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxGame;
 import openfl.display.Sprite;
 
 class Main extends Sprite
 {
+	public static inline final pixel_mult:Int = 5;
+
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, PlayState));
+
+		FlxG.save.bind('Spellbound');
+		addChild(new FlxGame(0, 0, PlayState, 60, 60, true, false));
 	}
 }
